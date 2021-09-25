153ad8421463aeb48bf24b18e313326aa75f99af
andrianjafimario <andrianjafimario@gmail.com>
2021-09-25 13:50:49 +0300

    slide 3

---
+++ components/Slide3/Slide3.js
@@ -0,0 +1,12 @@
+function createSlide3(parentElement){
+    var slide3=`<div class="slide3-container">
+        <div class="slide3">
+            <div class="text-in-slide">
+                <h1>Mary Ann</h1> <h1>Hamilton</h1><br/>
+                <span>AS <strong>JUDY CLEIN</strong></span><br/>
+            </div>
+        </div>
+    </div>`;
+    parentElement.innerHTML += slide3;
+}
+ 
--- pages/Home.js
+++ pages/Home.js
@@ -3,4 +3,6 @@ function createHomePage () {
     document.getElementById('main').innerHTML = homePage;
     var homeCtn = document.getElementById('homeContainer');
     homeCtn.innerHTML += '<h1>Hello, welcome to frond end awards 2021</h1>'
+    homeCtn.innerHTML += `<div id='slide-container'></div>`
+    createSlide3(document.getElementById("slide-container"));
 }
